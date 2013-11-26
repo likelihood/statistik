971386d5f25d7e2fce8f746d8bbfb3bf81a05ba6
Ketut <artayasa@gmail.com>
2013-11-26 20:50:26 +0800

    Update statistik.go

--- statistik.go
+++ statistik.go
@@ -3,3 +3,10 @@ import (
         "fmt"
         "math"
 )
+
+func Mean(data Interface) (mean float64){
+        Len := data.Len()
+        for i:=0;i<Len;i++{
+                mean += (data.Value(i) - mean) / float64(i+1)
+        }
+}
