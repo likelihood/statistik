dec5e026b8e5d10ee0317290bfb9b1ee569c1bb2
Ketut <artayasa@gmail.com>
2014-08-02 00:40:29 +0800

    Create statistik.go

---
+++ statistik.go
@@ -0,0 +1,17 @@
+package statistik
+
+import (
+	"fmt"
+	"math"
+)
+
+// rata-rata dari himpunan integer cth: 1,3,4,6,2,3,4
+func Rerata(himpunan []int) (rerata float64) {
+	if len(himpunan) == 0 {
+		return 0.0
+	}
+	for _, v := range himpunan {
+		rerata += float64(v)
+	}
+	return mean / float64(len(himpunan))
+}
